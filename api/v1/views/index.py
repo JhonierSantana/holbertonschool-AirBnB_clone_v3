179e233c0834964d2806dce97410eee2de2349df
root <root@DESKTOP-ISQ43BP>
2023-04-10 12:44:52 -0500

    upgrade

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
 """Index file for Flask"""
 
-
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def r_json():
     """a route for return JSON status """
     return jsonify(status='OK')
+
+
+@app_views.route('/stats', strict_slashes=False)
+def count_clases():
+    classes = {"amenities": Amenity, "cities": City,
+               "places": Place, "reviews": Review,
+               "states": State, "users": User}
+    ret_dict = dict()
+    for name, cls in classes.items():
+        ret_dict[name] = storage.count(cls)
+    return (jsonify(ret_dict))
