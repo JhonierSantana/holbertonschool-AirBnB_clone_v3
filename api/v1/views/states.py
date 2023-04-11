697a346881913912bed1b134ad88c37a7a96b0d5
root <root@DESKTOP-ISQ43BP>
2023-04-11 09:40:20 -0500

    upgrade

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -10,6 +10,9 @@ from models.state import State
 
 @app_views.route('/api/v1/states/', methods=['GET'], strict_slashes=False)
 def retrieves_states():
+    """
+    Retrieve list of all State objects.
+    """
     empy_list = []
     for state in storage.all(State).values():
         empy_list.append(state.to_dict())
@@ -18,6 +21,11 @@ def retrieves_states():
 
 @app_views.route('/api/v1/states/<state_id>', methods=['GET'], strict_slashes=False)
 def retrieves_state_by_id(state_id):
+    """
+    Delete State object with given id
+    Raise 404 error if id not linked to any State object
+    Returns and empty dictionary with status code 200
+    """
     for state in storage.all(State).values():
         if state.id == state_id:
             return jsonify(state.to_dict())
@@ -36,6 +44,13 @@ def deletes_state_by_id(state_id):
 
 @app_views.route('/api/v1/states', methods=['POST'], strict_slashes=False)
 def create_state():
+    """
+    Create State via POST
+    If HTTP body request is not valid JSON, raise 400 error, Not JSON
+    If dictionary doesn't contain key name, raise 400 error with
+    message Missing name
+    Return new State with status code 201
+    """
     new_state_dict = request.get_json()
     if not new_state_dict:
         abort(400, 'Not a JSON')
@@ -48,6 +63,13 @@ def create_state():
 
 @app_views.route('/api/v1/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state_by_id(state_id):
+    """
+    Update State object via PUT
+    If the state_id is not linked to any State object, raise 404 error
+    If the HTTP body request is not valid JSON, raise 400 error, Not a JSON
+    Update the State object with all key-value pairs of dictionary
+    Ignore keys: id, created_at, updated_at   
+    """
     state = storage.get(State, state_id)
     if not state:
         abort(404)
