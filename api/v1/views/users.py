0d7c792ab154240de5ba1268b4f4e12f0795ae7c
Esteban Enriquez <juan.esteban.94@outlook.com>
2023-04-12 07:43:50 -0500

    task 10

---
+++ api/v1/views/users.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""User flask handler"""
+
+
+from api.v1.views import app_views
+from flask import jsonify, abort, request, make_response
+from models import storage
+from models.user import User
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def users():
+    users = [user.to_dict() for user in storage.all(User).values()]
+    return jsonify(users)
+
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def user_by_id(user_id):
+    user = storage.get(User, user_id)
+    if user:
+        return jsonify(user.to_dict())
+    abort(404)
+
+
+@app_views.route(
+    '/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def deletes_user_by_id(user_id):
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    user.delete()
+    storage.save()
+    return jsonify(), 200
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    new_user_dict = request.get_json()
+    if not new_user_dict:
+        abort(400, 'Not a JSON')
+    if 'name' not in new_user_dict.keys():
+        abort(400, 'Missing name')
+    new_user = User(**new_user_dict)
+    new_user.save()
+    return jsonify(new_user.to_dict()), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_user_by_id(user_id):
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+
+    body = request.get_json()
+    if not body:
+        abort(400, 'Not a JSON')
+
+    for key, value in body.items():
+        if key in ['id', 'created_at', 'updated_at']:
+            continue
+        else:
+            setattr(user, key, value)
+    storage.save()
+    return jsonify(user.to_dict()), 200
